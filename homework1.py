ba059eb6b046b8fdc3a079b131478b2159dce0c2
Leha1007 <alukasenko08@gmail.com>
2024-08-03 15:26:02 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as div
+from true_math import divide as di
+
+
+result1 = div(69, 3)
+result2 = div(3, 0)
+result3 = di(49, 7)
+result4 = di(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
